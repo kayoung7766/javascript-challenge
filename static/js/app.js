e47292f6673cce3aa1874f88412248e6cfbadc68
Katie <kayoung7766@gmail.com>
2020-09-14 13:40:32 -0500

    table renders

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+// creating table
+
+var tbody = d3.select("tbody");
+
+data.forEach((i) => {
+  var row = tbody.append("tr");
+  Object.entries(i).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+});
+});
+
+//creating filtering on Date Search
+
